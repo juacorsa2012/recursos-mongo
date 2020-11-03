ef5041f4e387532b997b7aa02c6cda5fd12b9502
juacorsa2012 <juacorsa2012@gmail.com>
2020-11-03 20:16:13 +0100

    Se mejoran las validaciones de los libros y se actualizan los tests correspondientes

--- controllers/libros.controller.js
+++ controllers/libros.controller.js
@@ -5,6 +5,9 @@ const message = require('../utils/message')
 const ErrorResponse = require('../utils/errorResponse')
 const asyncHandler = require('../middlewares/async')
 const {añoActual} = require('../utils/helpers')
+const Tema = require('../models/tema.model')
+const Editorial = require('../models/editorial.model')
+const Idioma = require('../models/idioma.model')
 
 // @desc    Obtiene los libros registrados
 // @route   GET /api/v1/libros
@@ -63,10 +66,22 @@ const registrarLibro = asyncHandler(async (req, res, next) => {
 // @route   PUT /api/v1/libros/:id
 // @access  Public
 const actualizarLibro = asyncHandler(async (req, res, next) => {  
-  const { publicado } = req.body
+  const { publicado, tema, idioma, editorial } = req.body
 
   if (publicado > añoActual()) {
     return next(new ErrorResponse(message.PUBLICADO_MAXIMO, statusCode.BAD_REQUEST))
+  }
+  
+  if (!await Tema.findById(tema)) {    
+    return next(new ErrorResponse(message.TEMA_NO_ENCONTRADO, statusCode.BAD_REQUEST))
+  }  
+  
+  if (!await Idioma.findById(idioma)) {    
+    return next(new ErrorResponse(message.IDIOMA_NO_ENCONTRADO, statusCode.BAD_REQUEST))
+  }
+  
+  if (!await Editorial.findById(editorial)) {    
+    return next(new ErrorResponse(message.EDITORIAL_NO_ENCONTRADA, statusCode.BAD_REQUEST))
   }
 
   const id = req.params.id   
@@ -78,7 +93,7 @@ const actualizarLibro = asyncHandler(async (req, res, next) => {
       
   libro = await Libro.findByIdAndUpdate(id, req.body, {
     new: true,
-      runValidators: true
+    runValidators: true
   })  
             
   res.status(statusCode.OK).json({
--- tests/int/libros.controller.test.js
+++ tests/int/libros.controller.test.js
@@ -1,4 +1,5 @@
 const request = require('supertest')
+const mongoose = require('mongoose')
 const statusCode = require('http-status-codes')
 const Editorial  = require('../../models/editorial.model')
 const Tema   = require('../../models/tema.model')
@@ -173,7 +174,98 @@ describe('/api/v1/libros', () => {
         expect(res.body.error).toBe(message.PUBLICADO_MAXIMO)
       })
 
+  })
+
+  describe('PUT /', () => {   
+    let editorial
+    let idioma
+    let tema
+    let libro
+
+    beforeEach(async () => {
+      await clearDB()        
+      editorial = await Editorial.create({ nombre: 'Editorial 1'})  
+      idioma = await Idioma.create({ nombre: 'Idioma 1'})  
+      tema   = await Tema.create({ nombre: 'Tema 1'})  
+      datos = {
+        "titulo"    : "Titulo libro",
+        "paginas"   : 166,
+        "publicado" : añoActual(),
+        "tema"      : tema._id,
+        "editorial" : editorial._id,
+        "idioma"    : idioma._id
+      }
+
+      libro = await Libro.create(datos)
+    })
+
+    it("debe actualizar un libro", async () => {
+      datos.titulo = datos.titulo + ' actualizado'      
+
+      const res = await request(app).put(url + libro._id).send(datos)
+
+      expect(res.statusCode).toBe(statusCode.OK)
+      expect(res.body.status).toBe(message.SUCCESS)
+      expect(res.body.message).toBe(message.LIBRO_ACTUALIZADO)
+      expect(res.body.data.libro.titulo).toBe(datos.titulo)   
+      expect(res.body.data.libro._id).toBeDefined()        
+    })
+
+    it("debe devolver un error 400 si el año de publicación es posterior al año en curso", async () => {
+      datos.publicado = añoActual() + 1
+
+      const res = await request(app).put(url + libro._id).send(datos)      
+
+      expect(res.statusCode).toBe(statusCode.BAD_REQUEST)
+      expect(res.body.error).toBe(message.PUBLICADO_MAXIMO)
+    })
+
+    it("debe devolver un error 400 si el tema del libro no existe", async () => {
+      datos.tema = mongoose.Types.ObjectId()     
+
+      const res = await request(app).put(url + libro._id).send(datos)      
+
+      expect(res.statusCode).toBe(statusCode.BAD_REQUEST)
+      expect(res.body.error).toBe(message.TEMA_NO_ENCONTRADO)
+    })
+
+    it("debe devolver un error 400 si el idioma del libro no existe", async () => {
+      datos.idioma = mongoose.Types.ObjectId()     
+
+      const res = await request(app).put(url + libro._id).send(datos)      
+
+      expect(res.statusCode).toBe(statusCode.BAD_REQUEST)
+      expect(res.body.error).toBe(message.IDIOMA_NO_ENCONTRADO)
+    })
+
+    it("debe devolver un error 400 si la editorial del libro no existe", async () => {
+      datos.editorial = mongoose.Types.ObjectId()     
+
+      const res = await request(app).put(url + libro._id).send(datos)      
+
+      expect(res.statusCode).toBe(statusCode.BAD_REQUEST)
+      expect(res.body.error).toBe(message.EDITORIAL_NO_ENCONTRADA)
+    })
+
+    it("debe devolver un error 400 si el libro no existe", async () => {
+      const id = mongoose.Types.ObjectId()     
+
+      const res = await request(app).put(url + id).send(datos)      
+
+      expect(res.statusCode).toBe(statusCode.NOT_FOUND)
+      expect(res.body.error).toBe(message.LIBRO_NO_ENCONTRADO)
+    })
+
+    it("debe devolver un error 400 si no se facilita el título del libro", async () => {
+      datos.titulo = ''
+
+      const res = await request(app).put(url + libro._id).send(datos)      
+      
+      expect(res.statusCode).toBe(statusCode.BAD_REQUEST)
+      expect(res.body.error).toBe(message.TITULO_REQUERIDO)
     })
 
 
+
+  })
 })
